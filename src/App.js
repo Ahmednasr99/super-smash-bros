cc0333388edbac2de786c7acd659e160ff3543ae
Ahmed Nasr <eng_mimo98@yahoo.com>
2022-04-05 12:24:40 -0400

    fighter component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
+import Fighter from './componant/Fighter';
 // this is a component
 function App() {
   return (
     <div className="App">
       <h1>Fighters</h1>
-      <div className='fighters-grid'></div>
+      <div className='fighters-grid'>
+        <Fighter name={'roy'} color={'red'}/>
+        <Fighter name={'fox'} color={'yellow'}/>
+        <Fighter name={'gandolf'} color={'green'}/>
+      
+
+        
+
+      </div>
 
     </div>
   );
